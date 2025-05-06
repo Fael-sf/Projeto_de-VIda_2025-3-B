dc1fbbdc317e8b86f96d424a0d17ec50a104ba55
Rafael <145466519+Fael-sf@users.noreply.github.com>
2025-05-06 16:21:16 -0300

    06/05

--- main.js
+++ main.js
@@ -3,6 +3,12 @@ console.log(bptoes);
 
 for (
 
+for(let j = 0; j<botoes.length; j++){
+
+botoes[j].classList.remove("ativo");
+
+}
+
     (let i = 0; i < botoes.length; i++); {
 
     botoes[i].classList.add("ativo");
