12b7583c38b1ac2523cf1724701d0eead7d3afd7
Rafael <145466519+Fael-sf@users.noreply.github.com>
2025-05-06 16:13:55 -0300

    06/05

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
- const botoes = document.querySelectorAll(".botao");
- console.log(bptoes);
- 
+const botoes = document.querySelectorAll(".botao");
+console.log(bptoes);
+
+for (
+
+    (let i = 0; i < botoes.length; i++); {
+
+     botoes[i].classList.add("ativo");
+    }
+
+ )
