4a3b736a8421a409ddb097bc9b64dc52a5e26921
Rafael <145466519+Fael-sf@users.noreply.github.com>
2025-05-13 15:35:41 -0300

    13/05

--- main.js
+++ main.js
@@ -1,17 +1,13 @@
 const botoes = document.querySelectorAll(".botao");
 console.log(bptoes);
 
-for {
+for (let i = 0; i < botoes.length; i++); {
 
-for(let j = 0; j<botoes.length; j++){
+    for (letj = 0; j < botoes.length; j++) {
 
-botoes[j].classList.remove("ativo");
+        botoes[j].classList.remove("ativo");
 
-}
-
-    (let i = 0; i < botoes.length; i++); {
+    }
 
     botoes[i].classList.add("ativo");
-}
-
 }
