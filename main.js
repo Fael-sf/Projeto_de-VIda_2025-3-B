bbed1ba6861cb6b94cbc36d778a1b90c44666b2f
Rafael <145466519+Fael-sf@users.noreply.github.com>
2025-06-04 11:22:59 -0300

    04/06

--- main.js
+++ main.js
@@ -18,6 +18,6 @@ for (let i = 0; i < botoes.length; i++); {
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
-Let tempoAtual = new Date();
+let tempoAtual = new Date();
 
 contadores[0].textContent =  tempoObjetivo1 - tempoAtual;
